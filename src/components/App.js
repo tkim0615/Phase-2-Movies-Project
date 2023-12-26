2053fd4a508022d6b40216ebe4923081b12646ad
tyler kim <yoonsoo.kim11@gmail.com>
2023-12-26 13:06:48 -0500

    added router

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import '../App.css';
 import MoviePage from './MoviePage.jsx'
+import MovieList from './MovieList.jsx'
+import { Routes, Route } from'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <MoviePage />
+      <Routes>
+        <Route path="/" element={<MoviePage />} />
+        <Route path="/movies" element={<MovieList />} />
+      </Routes>
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,7 +2,7 @@
 import MovieCard from "./MovieCard";
 
 function MovieList({movies}) {
-    console.log(movies)
+
   return (
     <ul className="cards">{movies.map(movie=> <MovieCard key={movie.id} movie={movie} />)}</ul>
   );
