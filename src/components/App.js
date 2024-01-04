af9a5325840c7da698a6925ab2e080f6d1a73daa
Jeffrey Davis <j.davis081248@gmail.com>
2024-01-03 23:25:49 -0600

    AddNewMovieForm component now accesses public API for movie poster and release date. Tested and working.  Will have to check everyone elses to see how that works with the API key.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,5 @@
 import '../App.css';
 import MoviePage from './MoviePage.jsx'
-// import MovieList from './MovieList.jsx'
-import { Routes, Route } from'react-router-dom';
-// import NewMovieForm from './NewMovieForm.jsx';
 import Header from "./Header.jsx"
 
 function App() {
